545575e0316964e46b5b597a49eb91b609299d79
Vani Murarka <vani.murarka@gmail.com>
2019-08-16 14:24:00 +0530

    Update shabd-sampadaa.php

--- config/shabd-sampadaa.php
+++ config/shabd-sampadaa.php
@@ -9,7 +9,7 @@ return array(
 	|	|
 	*/
 
-	'api-base-url' => "http://localhost/ss5/public/api/",
+	'api-base-url' => env('API_URL', 'http://localhost/ss5/public/api/'),
 	'api-key' => env('API_KEY', 'do3bG3TXb2LO6TxP0NFYgQcOyNMJbvOt'),
 
 );
